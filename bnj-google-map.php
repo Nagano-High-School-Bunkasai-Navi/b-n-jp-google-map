5c76c0dd44e7002a73a4636a0f3e3e5fab9187f3
sugiyama <sugiyama@masato.nagano.jp>
2016-09-04 09:24:14 +0900

    Fix Code Style

--- bnj-google-map.php
+++ bnj-google-map.php
@@ -1,13 +1,15 @@
 <?php
-/*
-Plugin Name: 長野県高等学校文化祭Navi Google Map 管理
-Plugin URI: https://github.com/smasato/b-n-jp-google-map
-Description: 長野県高等学校文化祭Naviの Google Map 管理を行います
-Version: 0.1
-Author: Masato Sugiyama
-Author URI: https://github.com/smasato
-License: GPL2
+/**
+ * Plugin Name: 長野県高等学校文化祭Navi Google Map 管理
+ * Plugin URI: https://github.com/smasato/b-n-jp-google-map
+ * Description: 長野県高等学校文化祭Naviの Google Map 管理を行います
+ * Version: 0.1
+ * Author: Masato Sugiyama
+ * Author URI: https://github.com/smasato
+ * License: GPL2
+ */
 
+/**
 Copyright 2016 Masato Sugiyama (email : sugiyama@masato.nagano.jp)
 
 This program is free software; you can redistribute it and/or modify
@@ -22,5 +24,5 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-?>
+ */
+
